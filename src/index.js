2175a83212332c62466439694e73b7d04c17c5e8
Zach Morgan <zachmorgan97@gmail.com>
2023-06-10 12:55:29 -0700

    initial express app

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res, next) => {
+	res.send('Hello from service_1');
+});
+
+app.get('/users/:userId', async (req, res) => {
+  const options = {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+    },
+  };
+
+  // request user data from user_service
+  // update URL to match deployed user_service
+  const userRes = await fetch(`http://localhost:3001/users/${req.params.userId}`, options);
+  const user = await userRes.json();
+
+  // leave evidence that the data passed through this service
+  user.touchedBy.push('service_1');
+
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(user));
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
