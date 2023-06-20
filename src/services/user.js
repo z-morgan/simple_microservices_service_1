97550eec855162c3f00a846ce0ac437551c70fbf
Zach Morgan <zachmorgan97@gmail.com>
2023-06-19 19:24:11 -0700

    created consumer pact test for users/1

--- src/services/user.js
+++ src/services/user.js
@@ -13,6 +13,17 @@ class UserService {
   
     return await fetch(`${this.url}/users/${id}`, options);
   }
+
+  async getPosts() {
+    const options = {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+      },
+    };
+  
+    return await fetch(`${this.url}/posts`, options);
+  }
 }
 
 export default UserService;
